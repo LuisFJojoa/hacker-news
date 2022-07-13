0ab4e88e3f4f719cab311ffe3caaa63180fcc1a3
Fernando <luis.jojoa.q@gmail.com>
2022-07-13 02:55:10 -0500

    gh-pages integrated

--- src/components/HackerNewsHeader/HackerNewsHeader.jsx
+++
@@ -1,9 +0,0 @@
-import "./styles.css";
-export const HackerNewsHeader = () => {
-  return (
-    // hn --> Hacker News
-    <div className="hn-header">
-      <span className="hn-header-text">HACKER NEWS</span>
-    </div>
-  );
-};
--- src/helpers/convertDateToHours.js
+++
@@ -1,9 +0,0 @@
-export const getHoursToCreated = (dataString) =>
-  diffHours(new Date().getTime(), new Date(dataString).getTime());
-
-const diffHours = (currentDate, postDate) => {
-  const result = Math.abs((currentDate - postDate) / 1000 / (60 * 60));
-  return result < 0.5
-    ? `${Math.round(result * 60)} minutes ago`
-    : (Math.round(result) === 1)? `${Math.round(result)} hour ago`:`${Math.round(result)} hours ago` ;
-};
